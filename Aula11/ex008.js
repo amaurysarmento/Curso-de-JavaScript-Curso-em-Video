d4d85384ec97de04271f9c614a505443c0e98a92
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-02-22 18:35:49 -0300

    Condicionais Simples e Compostas

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+
+if (vel > 60) { // Condicional Simples
+    console.log('A velocidade permitida é de 60 km/h. Você foi multado.')
+    console.log(`A velocidade do seu carro foi de ${vel} km/h`)
+}
+
+console.log('Use sinto de segurança ao dirigir')
