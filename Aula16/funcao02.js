9c63a2a56b6f045b6166bc1d57a539c2c26eecd9
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-03-02 16:07:14 -0300

    funções

---
+++ Aula16/funcao02.js
@@ -0,0 +1,19 @@
+function soma (num1 = 0, num2 = 0) {
+    return num1 + num2
+}
+
+console.log(soma(4, 6))
+
+/*
+ Essa estrutura "num1 = 0, num2 = 0" dentro de um parâmento é chamado de parâmentros opcionais.
+*/
+
+
+/*
+function soma (num1, num2) {
+    return num1 + num2
+}
+
+console.log(soma(4, 6))
+*/
+
---
+++ Aula16/funcao03.js
@@ -0,0 +1,8 @@
+// jogando uma função dentro de uma variável.
+let v = function (x) {
+    return x * 2
+}
+
+console.log(v(5))
+
+//a função x vai pergar o valor (no caso o 5) e vai retornar ele multiplicado por 2
